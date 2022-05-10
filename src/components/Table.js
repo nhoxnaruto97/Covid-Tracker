4ef0f5c6bd9c23c22716d4481dd28b20fe9b94b3
nguyenCD <dinhnguyen.k15@gmail.com>
2022-05-11 01:00:55 +0700

    [Implement] - Implement Map component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import React from "react";
+import numeral from "numeral";
 import "../css/table.css";
 
 const Table = ({ countries }) => {
@@ -8,7 +9,7 @@ const Table = ({ countries }) => {
         <tr>
           <td>{country}</td>
           <td>
-            <strong>{cases}</strong>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
           </td>
         </tr>
       ))}
