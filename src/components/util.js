b60ee80a76d70a1a0a06de835a1121a06ae7e901
nguyenCD <dinhnguyen.k15@gmail.com>
2022-05-11 15:24:54 +0700

    [Fix] - Fix unique Id warning on browser

--- src/components/util.js
+++ src/components/util.js
@@ -41,6 +41,7 @@ export const prettyPrintStat = (stat) =>
 export const showDataOnMap = (data, casesType = "cases") =>
   data.map((country) => (
     <Circle
+      key={country.country}
       center={[country.countryInfo.lat, country.countryInfo.long]}
       color={casesTypeColors[casesType].hex}
       fillColor={casesTypeColors[casesType].hex}
